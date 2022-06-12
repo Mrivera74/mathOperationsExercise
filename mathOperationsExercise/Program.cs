5ad600715ab3894206c274fb66f032af347e85fb
Mark Rivera <mrivera7475@gmail.com>
2022-06-12 09:52:49 -0400

    updated code for exercise 2

--- mathOperationsExercise/Program.cs
+++ mathOperationsExercise/Program.cs
@@ -1,16 +1,44 @@
-﻿using System; 
+﻿using System;
 
 namespace mathOperationExercise
+
 {
-    class program
+    class Program
     {
         static void Main (string [] args)
         {
-            Console.WriteLine("Enter number");
-           int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(num1 * num2);     
+            int D = 17;
+
+            int V = 4;
+
+            int module = Module (D, V);
+
+            int quotient = Quotient (D,V);
+            Console.WriteLine($"{17}/{4}={quotient} R {module}");
+
+            double pi = 3.14;
+
+            Console.WriteLine("Enter Raduis");
+            double R = double.Parse (Console.ReadLine());
+
+            double A = Area(pi, R);
+
+            Console.WriteLine($" pi x {R}^2 = {A}");
+        }
+
+        public static int Quotient (int D, int V)
+        {
+            return D / V;
+        }
+
+        public static int Module (int D, int V)
+        {
+            return D % V;
+        }
+
+        public static double Area (double pi, double R)
+        {
+            return pi * (R *R);
         }
     }
 }
